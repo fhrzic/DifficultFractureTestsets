d503ec5c5329bfe84cd4d84d0b3f914441cabf5d
fhrzic <franko.hrzic@uniri.hr>
2025-06-04 20:19:17 +0200

    env name change

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="HardCaseEnv",
+    name="DifficultFractureTestsets",
     version="0.1.0",
     description="Code for the paper: Artificial Intelligence Test Set Performance in Difficult Cases Matched with Routine Cases of Pediatric Appendicular Skeleton Fractures",
     author="Your Name",
